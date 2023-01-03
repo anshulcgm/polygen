ec258356fb14b3cead32ea26d6bfde9f023d6d03
Anshul Ahluwalia <anshul.cgm@gmail.com>
2023-01-03 14:07:06 -0500

    Cleaned up class inference file for vertex model

--- polygen/inference/test_class_conditional_models.py
+++ polygen/inference/test_class_conditional_models.py
@@ -2,6 +2,8 @@ from typing import Dict, Union
 import os
 import pdb
 
+import matplotlib.pyplot as plt
+
 import torch
 import pytorch_lightning as pl
 from pytorch3d.io import save_obj
@@ -10,8 +12,9 @@ import hydra
 from hydra.utils import instantiate
 
 from polygen.polygen_config import VertexModelConfig, FaceModelConfig
+import polygen.utils.data_utils as data_utils
 
-CHECKPOINT_FILE = "/srv/share2/aahluwalia30/polygen/lightning_logs/version_491089/checkpoints/trained_vertex_model.ckpt"
+VERTEX_MODEL_CHECKPOINT_FILE = "/srv/share2/aahluwalia30/polygen/lightning_logs/version_491089/checkpoints/trained_vertex_model.ckpt"
 
 
 def sample_from_vertex_model(
@@ -22,7 +25,7 @@ def sample_from_vertex_model(
     vertex_model.eval()
     with torch.no_grad():
         vertex_samples = vertex_model.sample(
-            context=context, num_samples=num_samples, only_return_complete=False, max_sample_length=150
+            context=context, num_samples=num_samples, only_return_complete=False, max_sample_length=200
         )
     return vertex_samples
 
@@ -31,7 +34,7 @@ def load_vertex_model(config: VertexModelConfig) -> pl.LightningModule:
     """Loads vertex model from config object and .ckpt file"""
     model = config.vertex_model
     model.load_from_checkpoint(
-        CHECKPOINT_FILE,
+        VERTEX_MODEL_CHECKPOINT_FILE,
         decoder_config=model.decoder_config,
         quantization_bits=model.quantization_bits,
         class_conditional=model.class_conditional,
@@ -66,11 +69,25 @@ def write_vertices_to_obj_files(samples: Dict[str, torch.Tensor], directory_name
         save_path = os.path.join(directory_name, f"{i}.obj")
         save_obj(save_path, curr_verts, curr_faces)
 
+def plot_vertices(samples: Dict[str, torch.Tensor]) -> None:
+    """Plot generated vertices using matplotlib"""
+    vertices = samples["vertices"]
+    num_vertices = samples["num_vertices"]
+    mesh_list = []
+    for i in range(vertices.shape[0]):
+        curr_verts = vertices[i, :num_vertices[i]].numpy()
+        mesh_list.append({'vertices': curr_verts})
+    
+    data_utils.plot_meshes(mesh_list, ax_lims = 0.5)
+    plt.savefig('generated_meshes.png')
 
-if __name__ == "__main__":
-    pdb.set_trace()
-    vertex_model_config = load_config(config_name="vertex_model_config_1231.yaml", vertex_config=True)
+def test_vertex_model(config_name: str) -> None:
+    """Intitializes vertex model and samples from it"""
+    vertex_model_config = load_config(config_name=config_name, vertex_config = True)
     model = load_vertex_model(vertex_model_config)
     context = {"class_label": torch.Tensor([0, 1, 2, 3])}
     samples = sample_from_vertex_model(model, context)
-    write_vertices_to_obj_files(samples, "generated_meshes/")
+    plot_vertices(samples)
+
+if __name__ == "__main__":
+    test_vertex_model(config_name = "vertex_model_config_1231.yaml")
